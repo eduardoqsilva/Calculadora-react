f39d9d261e5ddbfdf3926c114ab89c65bc4f1f4d
eduardoqsilva <eduadsilva@outlook.com>
2023-03-10 16:22:16 -0300

    update main and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { ThemeProvider } from "styled-components"
+import { Header } from "./components/Header"
+import { Wrapper } from "./components/Wrapper"
+
+import { dark } from "./styles/themes/darkTheme"
+
+
 function App() {
 
   return (
-    <h1>hello</h1>
+    <ThemeProvider theme={dark}>
+      <Header>
+        <Wrapper>
+          
+        </Wrapper>
+      </Header>
+    </ThemeProvider>
   )
 }
 
