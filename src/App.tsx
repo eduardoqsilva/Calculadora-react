f29398c402eab97706a7d7b342ce77defcb7245a
eduardoqsilva <eduadsilva@outlook.com>
2023-03-10 21:49:55 -0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react"
 import { ThemeProvider } from "styled-components"
 import { Header } from "./components/Header"
+import { HeaderThemeSwith } from "./components/ThemeSwitch"
 import { Wrapper } from "./components/Wrapper"
 
 import { dark } from "./styles/themes/darkTheme"
@@ -7,11 +9,17 @@ import { dark } from "./styles/themes/darkTheme"
 
 function App() {
 
+  const [theme, setTheme] = useState<number>(0)
+  const updateTheme = () => theme < 2 ? setTheme(prev => prev +1) : setTheme(0);
+
   return (
     <ThemeProvider theme={dark}>
       <Header>
         <Wrapper>
-          
+          <HeaderThemeSwith 
+            theme={theme}
+            updateTheme={updateTheme}
+          />
         </Wrapper>
       </Header>
     </ThemeProvider>
