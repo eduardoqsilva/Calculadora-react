293995ecf028272b9f921732e54236dbc437f654
eduardoqsilva <eduadsilva@outlook.com>
2023-03-11 16:58:03 -0300

    Update darkTheme.ts

--- src/styles/themes/darkTheme.ts
+++ src/styles/themes/darkTheme.ts
@@ -1,6 +1,11 @@
 export const dark = {
-  headerColor : '#3b4664',
-  switchColors : [ '#d03f30', '#252d44'],
-  headerFontColor : '#fff',
-  screenColors : ['#191f32', '#fdfdfd'],
+  contrastColor : ['#d03f30', '#647298'], //contrast01, contrast02
+  textButtonContrastColors: ['#fdfdfd', '#fdfdfd'], //contrastText01, contrastText02
+  ButtonShadow : ['#7e2b25', '#414c6b'], //ShadownContrast01, shadownContrast02
+  
+  switchBackground : '#252d44', //switchBackgroundColor
+  headerColor : '#3b4664', //header background-color
+  headerFontColor : '#fff', //text
+  screenColors : ['#191f32', '#fdfdfd'], // background, text
+  ButtonColors : ['#ece4dd', '#4a4b54', '#797477'], //background, text, shadow
 }
