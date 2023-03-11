df5871ebc7ea3213366a8128906440f6f4616176
eduardoqsilva <eduadsilva@outlook.com>
2023-03-11 13:38:37 -0300

    update darkTheme.ts

--- src/styles/themes/darkTheme.ts
+++ src/styles/themes/darkTheme.ts
@@ -1,5 +1,6 @@
 export const dark = {
   headerColor : '#3b4664',
   switchColors : [ '#d03f30', '#252d44'],
-  headerFontColor : '#fff'
+  headerFontColor : '#fff',
+  screenColors : ['#191f32', '#fdfdfd'],
 }
